2948d1e99146732d070296ad6a47b88775dec0cc
jaegyeongkim <tkwk1205@naver.com>
2020-09-24 19:24:33 +0900

    Update Readme

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,47 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
+import App from "./App";
 
-import "assets/scss/material-kit-react.scss?v=1.9.0";
+import * as serviceWorker from "./serviceWorker";
 
-// pages for this product
-import Components from "views/Components/Components.js";
-import LandingPage from "views/LandingPage/LandingPage.js";
-import ProfilePage from "views/ProfilePage/ProfilePage.js";
-import LoginPage from "views/LoginPage/LoginPage.js";
-import Design from "views/Front/design.js";
-import ActorSearch from "views/Front/Actor/ActorSearch.js";
-import ActorSearchResult from "views/Front/Actor/ActorSearchResult.js";
-import ActorDetail from "views/Front/Actor/ActorDetail.js";
-import Category from "views/Front/Category/Category.js";
-import Introduce from "views/Front/Introduce/introduce.js";
-import Aboutus from "views/Front/AboutUs/aboutus.js";
-import License from "views/Front/License/license.js";
-import SignupPage from "views/SignupPage/SignupPage.jsx";
-// import Introduce from "views/Front/Introduce/introduce.js";
-
-var hist = createBrowserHistory();
-
-ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route path="/landing-page" component={LandingPage} />
-      <Route path="/profile-page" component={ProfilePage} />
-      <Route path="/login-page" component={LoginPage} />
-      <Route path="/design" component={Design} />
-      <Route path="/actor-search" component={ActorSearch} />
-      <Route path="/actor-search-result/:name" component={ActorSearchResult} />
-      <Route path="/actor-detail/:name" component={ActorDetail} />
-      <Route path="/category" component={Category} />
-      <Route path="/introduce" component={Introduce} />
-      <Route path="/about-us" component={Aboutus} />
-      <Route path="/license" component={License} />
-      <Route path="/signup-page" component={SignupPage} />
-
-      {/* !!!! 맨 밑에 있어야 동작합니다. */}
-      <Route path="/" component={Components} />
-    </Switch>
-  </Router>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
+serviceWorker.unregister();
--- frontend/src/views/Components/Components.js
+++ frontend/src/views/Components/Components.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 // nodejs library that concatenates classes
 import classNames from "classnames";
 // react components for routing our app without refresh
@@ -16,26 +16,16 @@ import Button from "components/CustomButtons/Button.js";
 import Parallax from "components/Parallax/Parallax.js";
 // sections for this page
 import HeaderLinks from "components/Header/HeaderLinks.js";
-import SectionBasics from "./Sections/SectionBasics.js";
-import SectionNavbars from "./Sections/SectionNavbars.js";
-import SectionTabs from "./Sections/SectionTabs.js";
-import SectionPills from "./Sections/SectionPills.js";
-import SectionNotifications from "./Sections/SectionNotifications.js";
-import SectionTypography from "./Sections/SectionTypography.js";
-import SectionJavascript from "./Sections/SectionJavascript.js";
-import SectionCarousel from "./Sections/SectionCarousel.js";
-import SectionCompletedExamples from "./Sections/SectionCompletedExamples.js";
-import SectionLogin from "./Sections/SectionLogin.js";
-import SectionExamples from "./Sections/SectionExamples.js";
-import SectionDownload from "./Sections/SectionDownload.js";
-import ListItem from "@material-ui/core/ListItem";
 import styles from "assets/jss/material-kit-react/views/components.js";
+
+import { CommonContext } from "../SignupPage/CommonContext.js";
 
 const useStyles = makeStyles(styles);
 
 export default function Components(props) {
   const classes = useStyles();
   const { ...rest } = props;
+  const { productDatas } = useContext(CommonContext);
   return (
     <div>
       <Header
@@ -71,6 +61,7 @@ export default function Components(props) {
                     simple
                   >
                     배우 추천 받아보기
+                    {productDatas}
                   </Button>
                 </Link>
                 <Link
@@ -95,27 +86,6 @@ export default function Components(props) {
         </div>
       </Parallax>
 
-      {/* <div className={classNames(classes.main, classes.mainRaised)}>
-        <SectionBasics />
-        <SectionNavbars />
-        <SectionTabs />
-        <SectionPills />
-        <SectionNotifications />
-        <SectionTypography />
-        <SectionJavascript />
-        <SectionCarousel />
-        <SectionCompletedExamples />
-        <SectionLogin />
-        <GridItem md={12} className={classes.textCenter}>
-          <Link to={"/login-page"} className={classes.link}>
-            <Button color="primary" size="lg" simple>
-              View Login Page
-            </Button>
-          </Link>
-        </GridItem>
-        <SectionExamples />
-        <SectionDownload />
-      </div> */}
       <Footer />
     </div>
   );
